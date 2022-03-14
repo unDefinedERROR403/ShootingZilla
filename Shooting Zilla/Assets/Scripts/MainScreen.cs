244d537bbae0d086706f298204f636bbfd84aeb5
undefinedERROR1 <mswaroop911@gmail.com>
2022-03-14 17:46:04 -0400

    Update 2
    Integration:
    1) Main Screen
    2) Credit Screen
    3) Fight Scene(Camera will open but need to add characters and game play functions.)

---
+++ Shooting Zilla/Assets/Scripts/MainScreen.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class MainScreen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void fight()
+    {
+        SceneManager.LoadScene("Game");
+    }
+    public void Settings()
+    {
+        SceneManager.LoadScene("Settings");
+    }
+    public void Credits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+}
